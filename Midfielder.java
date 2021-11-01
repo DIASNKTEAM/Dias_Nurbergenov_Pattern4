f600bcfc8c45dd1c8ddd44106b242b1f0c11bcee
DIASNKTEAM <52606234+DIASNKTEAM@users.noreply.github.com>
2021-11-01 15:16:11 +0600

    Delete Midfielder.java

--- Midfielder.java
+++
@@ -1,6 +0,0 @@
-public class Midfielder implements Player {
-    @Override
-    public void action() {
-        System.out.println("Midfielders turn into magicians and help attackers to destroy the opponents");
-    }
-}
