b58ae8279f73ceb74230f0a544fcb206836a47d0
DIASNKTEAM <52606234+DIASNKTEAM@users.noreply.github.com>
2021-11-01 15:15:32 +0600

    Delete Attacker.java

--- Attacker.java
+++
@@ -1,7 +0,0 @@
-public class Attacker implements Player {
-
-    @Override
-    public void action() {
-        System.out.println("Attackers easily destroy the opponents with their cold-blooded heads.");
-    }
-}
